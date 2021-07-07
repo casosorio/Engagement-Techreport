3a97a60f0d673524f1fc51b02a21ce65e95caa17
Your Name <you@example.com>
2021-07-06 23:32:21 -0400

    lots of changes

--- Scripts/All Scripts.R
+++ Scripts/All Scripts.R
@@ -124,6 +124,119 @@ cog.ded
 
 ## CFA Analysis
 
+library(lavaan)
+library(semPlot)
+
+CFAdata<-together[,2:37]
+
+
+CFAdata<-CFAdata%>%rename(
+  Item_1=`Iâ€™m able to concentrate on my work without distractions.`,
+  Item_2=`I have a hard time detaching mentally from my work.`,
+  Item_3=`Time passes quickly while Iâ€™m working.`,
+  Item_4=`I find it difficult to mentally disconnect from work.`,
+  Item_5=`I enjoy thinking about work even when Iâ€™m not at work.`,
+  Item_6=`Most days, I feel happiest when the workday is soon to be complete.`,
+  Item_7=`I am happiest when I am immersed in a project.`,
+  Item_8=`I love starting my workday.`,
+  Item_9=`I devote more time than is expected of me.`,
+  Item_10=`I have to be reminded to take breaks while Iâ€™m at work.`,
+  Item_11=`I never miss a work deadline.`,
+  Item_12=`I never allow distractions to interfere with my work.`,
+  Item_13=`I devote my full attention to my work tasks throughout the day.`,
+  Item_14=`Thinking about work saps my energy.`,
+  Item_15=`I would rather direct my focus toward a work task than a personal task.`,
+  Item_16=`Iâ€™m able to maintain good levels of energy throughout the workday.`,
+  Item_17=`I enjoy spending time completing my job tasks.`,
+  Item_18=`Most days I feel enthusiastic about starting my work day.`,
+  Item_19=`I feel motivated to go beyond what is asked of me.`,
+  Item_20=`This job drains my energy.`,
+  Item_21=`When work is slow I find ways to be productive.`,
+  Item_22=`I express enthusiasm for my job while at work.`,
+  Item_23=`I try my best to perform well at work.`,
+  Item_24=`If I notice my energy level is low, I take corrective steps to re-energize.`,
+  Item_25=`I plan my future with this company.`,
+  Item_26=`I believe this company cares about my career goals.`,
+  Item_27=`I often think about finding another job.`,
+  Item_28=`This organization challenges me to work at my full potential.`,
+  Item_29=`I am proud to be a member of this organization.`,
+  Item_30=`I feel supported by my supervisor when I fail at a task.`,
+  Item_31=`I feel proud of my accomplishments within this organization.`,
+  Item_32=`My job makes me feel like Iâ€™m part of something meaningful.`,
+  Item_33=`I make valued contributions to the organization.`,
+  Item_34=`I embrace challenging situations at work.`,
+  Item_35=`I speak positively about this organization to others.`,
+  Item_36=`This organization provides the resources necessary for me to successfully perform my job.`
+)
+
+
+Sub_Model<-'
+Absorption=~Item_1+Item_2+Item_3+Item_4+Item_5+Item_6+Item_7+Item_8+Item_9+Item_10+Item_11+Item_12
+Vigor=~Item_13+Item_14+Item_15+Item_16+Item_17+Item_18+Item_19+Item_20+Item_21+Item_22+Item_23+Item_24
+Dedication=~Item_25+Item_26+Item_27+Item_28+Item_29+Item_30+Item_31+Item_32+Item_33+Item_34+Item_35+Item_36
+'
+
+Fit1.1<-lavaan::cfa(Sub_Model, data = CFAdata)
+semPlot::semPaths(Fit1.1, "std")
+fit1.1 <- as.data.frame(fitMeasures(Fit1.1))
+summary(Fit1.1, fit.measure=TRUE)
+
+#write.csv(fit1.1, "temp.csv")
+
+## Looking at scale correlations because of very large latent covariances
+
+testing <- CFAdata
+
+testing$absorp <- rowMeans(testing[1:12], na.rm=TRUE)
+testing$vigor <- rowMeans(testing[13:24], na.rm=TRUE)
+testing$dedic <- rowMeans(testing[25:36], na.rm=TRUE)
+
+cor(testing[37:39], use="complete.obs")
+
+## plot(testing$absorp, testing$dedic)
+
+Att_Model<-'
+Cognitive=~Item_1+Item_2+Item_3+Item_4+Item_13+Item_14+Item_15+Item_16+Item_25+Item_26+Item_27+Item_28
+Affective=~Item_5+Item_6+Item_7+Item_8+Item_17+Item_18+Item_19+Item_20+Item_29+Item_30+Item_31+Item_32
+Behavioral=~Item_9+Item_10+Item_11+Item_12+Item_21+Item_22+Item_23+Item_24+Item_33+Item_34+Item_35+Item_36
+'
+Fit1.2<-lavaan::cfa(Att_Model, data = CFAdata)
+semPlot::semPaths(Fit1.2,"std")
+lavaan::fitMeasures(Fit1.2)
+summary(Fit1.2, fit.measure=TRUE)
+
+
+######Bi factor model
+Bifactor_Model<-'
+Cognitive=~Item_1+Item_2+Item_3+Item_4+Item_13+Item_14+Item_15+Item_16+Item_25+Item_26+Item_27+Item_28
+Affective=~Item_5+Item_6+Item_7+Item_8+Item_17+Item_18+Item_19+Item_20+Item_29+Item_30+Item_31+Item_32
+Behavioral=~Item_9+Item_10+Item_11+Item_12+Item_21+Item_22+Item_23+Item_24+Item_33+Item_34+Item_35+Item_36
+Absorption=~Item_1+Item_2+Item_3+Item_4+Item_5+Item_6+Item_7+Item_8+Item_9+Item_10+Item_11+Item_12
+Vigor=~Item_13+Item_14+Item_15+Item_16+Item_17+Item_18+Item_19+Item_20+Item_21+Item_22+Item_23+Item_24
+Dedication=~Item_25+Item_26+Item_27+Item_28+Item_29+Item_30+Item_31+Item_32+Item_33+Item_34+Item_35+Item_36
+Absorption ~~ 0*Affective
+Absorption ~~ 0*Behavioral
+Absorption ~~ 0*Cognitive
+Vigor ~~ 0*Affective
+Vigor ~~ 0*Behavioral
+Vigor ~~ 0*Cognitive
+Dedication ~~ 0*Affective
+Dedication ~~ 0*Behavioral
+Dedication ~~ 0*Cognitive
+'
+
+
+Fit.Bi <- lavaan::cfa(Bifactor_Model, data = CFAdata) #, likelihood = "wishart")
+
+semPlot::semPaths(Fit.Bi, bifactor = c("Cognitive", "Affective", "Behavioral"), "std", layout = "tree3", 
+                  rotation = 2, curvePivot=TRUE, style="lisrel", nCharNodes = 0)  ## exoCov=FALSE deletes all covariances
+title("Initial pilot bifactor analysis (36 candidate items)")
+
+modindices(Fit.Bi, sort = TRUE, maximum.number = 5)
+
+summary(Fit.Bi)
+
+
 
 #############Item Reduction-3 items #######################
 ## Aff-Abs
@@ -203,30 +316,81 @@ psych::alpha(together[,c(6,7,9,18,19,20,26,27,29)]) #alpha-.83
 ##### all together
 psych::alpha(together[,c(2,3,5,6,7,9,10,12,13,15,16,17,18,19,20,23,24,25,26,27,29,31,32,33,35,36,37)])
 
+Reduction_3Items<-together[,c(2,3,5,6,7,9,10,12,13,15,16,17,18,19,20,23,24,25,26,27,29,31,32,33,35,36,37)]
 ### Creating CFA for three item response 
-library(lavaan)
-library(semPlot)
+Reduction_3Items<-Reduction_3Items%>%rename(
+#  Item_1=`Iâ€™m able to concentrate on my work without distractions.`,
+  Item_2=`I have a hard time detaching mentally from my work.`,
+  Item_3=`Time passes quickly while Iâ€™m working.`,
+  Item_4=`I find it difficult to mentally disconnect from work.`,
+  Item_5=`I enjoy thinking about work even when Iâ€™m not at work.`,
+  Item_6=`Most days, I feel happiest when the workday is soon to be complete.`,
+#  Item_7=`I am happiest when I am immersed in a project.`,
+  Item_8=`I love starting my workday.`,
+#  Item_9=`I devote more time than is expected of me.`,
+  Item_10=`I have to be reminded to take breaks while Iâ€™m at work.`,
+  Item_11=`I never miss a work deadline.`,
+  Item_12=`I never allow distractions to interfere with my work.`,
+  Item_13=`I devote my full attention to my work tasks throughout the day.`,
+  Item_14=`Thinking about work saps my energy.`,
+#  Item_15=`I would rather direct my focus toward a work task than a personal task.`,
+  Item_16=`Iâ€™m able to maintain good levels of energy throughout the workday.`,
+  Item_17=`I enjoy spending time completing my job tasks.`,
+  Item_18=`Most days I feel enthusiastic about starting my work day.`,
+#  Item_19=`I feel motivated to go beyond what is asked of me.`,
+  Item_20=`This job drains my energy.`,
+  Item_21=`When work is slow I find ways to be productive.`,
+  Item_22=`I express enthusiasm for my job while at work.`,
+  Item_23=`I try my best to perform well at work.`,
+#  Item_24=`If I notice my energy level is low, I take corrective steps to re-energize.`,
+#  Item_25=`I plan my future with this company.`,
+  Item_26=`I believe this company cares about my career goals.`,
+  Item_27=`I often think about finding another job.`,
+  Item_28=`This organization challenges me to work at my full potential.`,
+  Item_29=`I am proud to be a member of this organization.`,
+#  Item_30=`I feel supported by my supervisor when I fail at a task.`,
+  Item_31=`I feel proud of my accomplishments within this organization.`,
+  Item_32=`My job makes me feel like Iâ€™m part of something meaningful.`,
+#  Item_33=`I make valued contributions to the organization.`,
+  Item_34=`I embrace challenging situations at work.`,
+  Item_35=`I speak positively about this organization to others.`,
+  Item_36=`This organization provides the resources necessary for me to successfully perform my job.`
+)
 
+Sub_Model_ItemReduction3<-'
+Absorption=~Item_2+Item_3+Item_4+Item_5+Item_6+Item_8+Item_10+Item_11+Item_12
+Vigor=~Item_13+Item_14+Item_16+Item_17+Item_18+Item_20+Item_21+Item_22+Item_23
+Dedication=~+Item_26+Item_27+Item_28+Item_29+Item_31+Item_32+Item_34+Item_35+Item_36
+'
 
+Fit1.3<-lavaan::cfa(Att_Model, data = CFAdata)
+semPlot::semPaths(Fit1.3,"std")
+lavaan::fitMeasures(Fit1.3)
+summary(Fit1.3, fit.measure=TRUE)
 
-
-
+Att_Model<-'
+Cognitive=~Item_1+Item_2+Item_3+Item_4+Item_13+Item_14+Item_15+Item_16+Item_25+Item_26+Item_27+Item_28
+Affective=~Item_5+Item_6+Item_7+Item_8+Item_17+Item_18+Item_19+Item_20+Item_29+Item_30+Item_31+Item_32
+Behavioral=~Item_9+Item_10+Item_11+Item_12+Item_21+Item_22+Item_23+Item_24+Item_33+Item_34+Item_35+Item_36
+'
+Fit1.2<-lavaan::cfa(Att_Model, data = CFAdata)
+semPlot::semPaths(Fit1.2,"std")
+lavaan::fitMeasures(Fit1.2)
+summary(Fit1.2, fit.measure=TRUE)
 
 ############### Item Reduction- 2 Items #######################
 
 ####### Aff-Abs
-psych::alpha(together[,c(2,3)],check.keys = T) # Second Best
-psych::alpha(together[,c(2,4)],check.keys = T) # Third Best
-psych::alpha(together[,c(3,4)],check.keys = T) # Bad
-psych::alpha(together[,c(3,5)],check.keys = T) # Fourth Best
-psych::alpha(together[,c(4,5)],check.keys = T) # Bad
-psych::alpha(together[,c(2,5)],check.keys = T) # Best 
-
-
+psych::alpha(together[,c(2,3)],check.keys = T) #alpha -.53, r.drops - .36, .36
+psych::alpha(together[,c(2,4)],check.keys = T) #alpha -.44, r.drops - .30, .30
+psych::alpha(together[,c(3,4)],check.keys = T) #alpha -.27, r.drops - .16, .16
+psych::alpha(together[,c(3,5)],check.keys = T) #alpha -.45, r.drops - .29, .29
+psych::alpha(together[,c(4,5)],check.keys = T) #alpha -.39, r.drops - .25, .25
+psych::alpha(together[,c(2,5)],check.keys = T) #alpha -.68, r.drops - .57, .57
 
 
 ####### Aff-Vig
-psych::alpha(together[,c(6,7)],check.keys = T) # Best
+psych::alpha(together[,c(6,7)],check.keys = T) #alpha -.70, r.drops -.56, .56
 psych::alpha(together[,c(6,8)],check.keys = T) # Second Best
 psych::alpha(together[,c(6,9)],check.keys = T) # Bad
 psych::alpha(together[,c(7,8)],check.keys = T) # Ok
