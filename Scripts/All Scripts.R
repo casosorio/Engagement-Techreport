9b29f56b74b5eb909e4a12127932caae1f397321
Your Name <you@example.com>
2021-07-07 10:52:02 -0400

    finished item reduction

--- Scripts/All Scripts.R
+++ Scripts/All Scripts.R
@@ -363,20 +363,54 @@ Vigor=~Item_13+Item_14+Item_16+Item_17+Item_18+Item_20+Item_21+Item_22+Item_23
 Dedication=~+Item_26+Item_27+Item_28+Item_29+Item_31+Item_32+Item_34+Item_35+Item_36
 '
 
-Fit1.3<-lavaan::cfa(Att_Model, data = CFAdata)
+Fit1.3<-lavaan::cfa(Sub_Model_ItemReduction3, data = Reduction_3Items)
 semPlot::semPaths(Fit1.3,"std")
 lavaan::fitMeasures(Fit1.3)
 summary(Fit1.3, fit.measure=TRUE)
 
-Att_Model<-'
-Cognitive=~Item_1+Item_2+Item_3+Item_4+Item_13+Item_14+Item_15+Item_16+Item_25+Item_26+Item_27+Item_28
-Affective=~Item_5+Item_6+Item_7+Item_8+Item_17+Item_18+Item_19+Item_20+Item_29+Item_30+Item_31+Item_32
-Behavioral=~Item_9+Item_10+Item_11+Item_12+Item_21+Item_22+Item_23+Item_24+Item_33+Item_34+Item_35+Item_36
+Att_Model_ItemReduction3<-'
+Cognitive=~Item_2+Item_3+Item_4+Item_13+Item_14+Item_16+Item_26+Item_27+Item_28
+Affective=~Item_5+Item_6+Item_8+Item_17+Item_18+Item_20+Item_29+Item_31+Item_32
+Behavioral=~Item_10+Item_11+Item_12+Item_21+Item_22+Item_23+Item_34+Item_35+Item_36
 '
-Fit1.2<-lavaan::cfa(Att_Model, data = CFAdata)
+Fit1.2<-lavaan::cfa(Att_Model_ItemReduction3, data = Reduction_3Items)
 semPlot::semPaths(Fit1.2,"std")
 lavaan::fitMeasures(Fit1.2)
 summary(Fit1.2, fit.measure=TRUE)
+
+
+#### Bifactor model 3 items ########
+Bifactor_Mode2<-'
+Cognitive=~Item_2+Item_3+Item_4+Item_13+Item_14+Item_16+Item_26+Item_27+Item_28
+Affective=~Item_5+Item_6+Item_8+Item_17+Item_18+Item_20+Item_29+Item_31+Item_32
+Behavioral=~Item_10+Item_11+Item_12+Item_21+Item_22+Item_23+Item_34+Item_35+Item_36
+Absorption=~Item_2+Item_3+Item_4+Item_5+Item_6+Item_8+Item_10+Item_11+Item_12
+Vigor=~Item_13+Item_14+Item_16+Item_17+Item_18+Item_20+Item_21+Item_22+Item_23
+Dedication=~+Item_26+Item_27+Item_28+Item_29+Item_31+Item_32+Item_34+Item_35+Item_36
+Absorption ~~ 0*Affective
+Absorption ~~ 0*Behavioral
+Absorption ~~ 0*Cognitive
+Vigor ~~ 0*Affective
+Vigor ~~ 0*Behavioral
+Vigor ~~ 0*Cognitive
+Dedication ~~ 0*Affective
+Dedication ~~ 0*Behavioral
+Dedication ~~ 0*Cognitive
+'
+
+
+Fit.Bi2 <- lavaan::cfa(Bifactor_Mode2, data = Reduction_3Items) #, likelihood = "wishart")
+
+semPlot::semPaths(Fit.Bi2, bifactor = c("Cognitive", "Affective", "Behavioral"), "std", layout = "tree3", 
+                  rotation = 2, curvePivot=TRUE, style="lisrel", nCharNodes = 0)  ## exoCov=FALSE deletes all covariances
+title("Initial pilot bifactor analysis (36 candidate items)")
+
+modindices(Fit.Bi2, sort = TRUE, maximum.number = 5)
+
+summary(Fit.Bi2)
+
+
+
 
 ############### Item Reduction- 2 Items #######################
 
@@ -391,113 +425,192 @@ psych::alpha(together[,c(2,5)],check.keys = T) #alpha -.68, r.drops - .57, .57
 
 ####### Aff-Vig
 psych::alpha(together[,c(6,7)],check.keys = T) #alpha -.70, r.drops -.56, .56
-psych::alpha(together[,c(6,8)],check.keys = T) # Second Best
-psych::alpha(together[,c(6,9)],check.keys = T) # Bad
-psych::alpha(together[,c(7,8)],check.keys = T) # Ok
-psych::alpha(together[,c(7,9)],check.keys = T) # Ok
-psych::alpha(together[,c(8,9)],check.keys = T) # Bad
+psych::alpha(together[,c(6,8)],check.keys = T) #alpha -.68, r.drops -.53, .53
+psych::alpha(together[,c(6,9)],check.keys = T) #alpha -.36, r.drops -.23, .23
+psych::alpha(together[,c(7,8)],check.keys = T) #alpha -.60, r.drops -.43, .43
+psych::alpha(together[,c(7,9)],check.keys = T) #alpha -.58, r.drops -.41, .41
+psych::alpha(together[,c(8,9)],check.keys = T) #alpha -.29, r.drops -.17, .17
 
 
 ####### Aff-Ded
-psych::alpha(together[,c(10,11)],check.keys = T) # Ok
-psych::alpha(together[,c(10,12)],check.keys = T) # Good
-psych::alpha(together[,c(10,13)],check.keys = T) # Good
-psych::alpha(together[,c(11,12)],check.keys = T) # Ok
-psych::alpha(together[,c(11,13)],check.keys = T) # Bad
-psych::alpha(together[,c(12,13)],check.keys = T) # Ok
+psych::alpha(together[,c(10,11)],check.keys = T) #alpha -.47, r.drops -.31, .31
+psych::alpha(together[,c(10,12)],check.keys = T) #alpha -.68, r.drops -.52, .52
+psych::alpha(together[,c(10,13)],check.keys = T) #alpha -.77, r.drops -.62, .62
+psych::alpha(together[,c(11,12)],check.keys = T) #alpha -.44, r.drops -.44, .44
+psych::alpha(together[,c(11,13)],check.keys = T) #alpha -.52, r.drops -.35, .35
+psych::alpha(together[,c(12,13)],check.keys = T) #alpha -.71, r.drops -.57, .57
 
 
 ###### Beh-Abs
-psych::alpha(together[,c(14,15)],check.keys = T) # Good
-psych::alpha(together[,c(14,16)],check.keys = T) # Bad
-psych::alpha(together[,c(14,17)],check.keys = T) # ok
-psych::alpha(together[,c(15,16)],check.keys = T) # Bad
-psych::alpha(together[,c(15,17)],check.keys = T) # Ok
-psych::alpha(together[,c(16,17)],check.keys = T) # Good
+psych::alpha(together[,c(14,15)],check.keys = T) #alpha -.56, r.drops -.40, .40
+psych::alpha(together[,c(14,16)],check.keys = T) #alpha -.04, r.drops -.02, .02
+psych::alpha(together[,c(14,17)],check.keys = T) #alpha -.45, r.drops -.29, .29
+psych::alpha(together[,c(15,16)],check.keys = T) #alpha -.22, r.drops -.13, .13
+psych::alpha(together[,c(15,17)],check.keys = T) #alpha -.39, r.drops -.24, .24
+psych::alpha(together[,c(16,17)],check.keys = T) #alpha -.51, r.drops -.34, .34
 
 
 ###### Beh-vig
-psych::alpha(together[,c(18,19)],check.keys = T) # Good
-psych::alpha(together[,c(18,20)],check.keys = T) # Ok
-psych::alpha(together[,c(18,21)],check.keys = T) # Ok
-psych::alpha(together[,c(19,20)],check.keys = T) # Ehhh
-psych::alpha(together[,c(19,21)],check.keys = T) # Ok
-psych::alpha(together[,c(20,21)],check.keys = T) # Bad
+psych::alpha(together[,c(18,19)],check.keys = T) #alpha -.60, r.drops -.43, .43
+psych::alpha(together[,c(18,20)],check.keys = T) #alpha -.56, r.drops -.43, .43
+psych::alpha(together[,c(18,21)],check.keys = T) #alpha -.49, r.drops -.33, .33
+psych::alpha(together[,c(19,20)],check.keys = T) #alpha -.63, r.drops -.49, .49
+psych::alpha(together[,c(19,21)],check.keys = T) #alpha -.50, r.drops -.33, .33
+psych::alpha(together[,c(20,21)],check.keys = T) #alpha -.44, r.drops -.31, .31
 
 
 
 ###### Bed-ded
-psych::alpha(together[,c(22,23)],check.keys = T) # Good
-psych::alpha(together[,c(22,24)],check.keys = T) # Bad
-psych::alpha(together[,c(22,25)],check.keys = T) # Bad
-psych::alpha(together[,c(23,24)],check.keys = T) # Ok
-psych::alpha(together[,c(23,25)],check.keys = T) # Ok
-psych::alpha(together[,c(24,25)],check.keys = T) # Good
+psych::alpha(together[,c(22,23)],check.keys = T) #alpha -.64, r.drops -.47, .47
+psych::alpha(together[,c(22,24)],check.keys = T) #alpha -.39, r.drops -.27, .27
+psych::alpha(together[,c(22,25)],check.keys = T) #alpha -.24, r.drops -.15, .15
+psych::alpha(together[,c(23,24)],check.keys = T) #alpha -.50, r.drops -.35, .35
+psych::alpha(together[,c(23,25)],check.keys = T) #alpha -.37, r.drops -.23, .23
+psych::alpha(together[,c(24,25)],check.keys = T) #alpha -.62, r.drops -.45, .45
 
 
 ###### Cog-Vig
-psych::alpha(together[,c(26,29)],check.keys = T) # Good
-psych::alpha(together[,c(27,29)],check.keys = T) # Good
-psych::alpha(together[,c(28,29)],check.keys = T) # Ok
-psych::alpha(together[,c(27,28)],check.keys = T) # Ok
-psych::alpha(together[,c(27,29)],check.keys = T) # Good
-psych::alpha(together[,c(28,29)],check.keys = T) # Ok
+psych::alpha(together[,c(26,29)],check.keys = T) #alpha -.65, r.drops -.49, .49
+psych::alpha(together[,c(27,29)],check.keys = T) #alpha -.62, r.drops -.45, .45
+psych::alpha(together[,c(28,29)],check.keys = T) #alpha -.45, r.drops -.29, .29
+psych::alpha(together[,c(27,28)],check.keys = T) #alpha -.26, r.drops -.15, .15
+psych::alpha(together[,c(27,29)],check.keys = T) #alpha -.62, r.drops -.45, .45
+psych::alpha(together[,c(28,29)],check.keys = T) #alpha -.45, r.drops -.29, .29
 
 ###### Cog-def
-psych::alpha(together[,c(30,31)],check.keys = T) # Good
-psych::alpha(together[,c(30,32)],check.keys = T) # Good
-psych::alpha(together[,c(30,33)],check.keys = T) # Ok
-psych::alpha(together[,c(31,32)],check.keys = T) # Ok
-psych::alpha(together[,c(31,33)],check.keys = T) # Good
-psych::alpha(together[,c(32,33)],check.keys = T) # Ok
+psych::alpha(together[,c(30,31)],check.keys = T) #alpha -.72, r.drops -.56, .56
+psych::alpha(together[,c(30,32)],check.keys = T) #alpha -.76, r.drops -.61, .61
+psych::alpha(together[,c(30,33)],check.keys = T) #alpha -.66, r.drops -.51, .51
+psych::alpha(together[,c(31,32)],check.keys = T) #alpha -.70, r.drops -.54, .54
+psych::alpha(together[,c(31,33)],check.keys = T) #alpha -.80, r.drops -.67, .67
+psych::alpha(together[,c(32,33)],check.keys = T) #alpha -.64, r.drops -.48, .48
 
 
 ###### Cog-abs
-psych::alpha(together[,c(34,35)],check.keys = T) # Bad
-psych::alpha(together[,c(34,36)],check.keys = T) # Good
-psych::alpha(together[,c(34,37)],check.keys = T) # Bad
-psych::alpha(together[,c(35,36)],check.keys = T) # Ok
-psych::alpha(together[,c(35,37)],check.keys = T) # Good
-psych::alpha(together[,c(36,37)],check.keys = T) # Bad
+psych::alpha(together[,c(34,35)],check.keys = T) #alpha -.02, r.drops -.01, .01
+psych::alpha(together[,c(34,36)],check.keys = T) #alpha -.58, r.drops -.42, .42
+psych::alpha(together[,c(34,37)],check.keys = T) #alpha -.04, r.drops -.02, .02
+psych::alpha(together[,c(35,36)],check.keys = T) #alpha -.25, r.drops -.15, .15
+psych::alpha(together[,c(35,37)],check.keys = T) #alpha -.94, r.drops -.89, .89
+psych::alpha(together[,c(36,37)],check.keys = T) #alpha -.16, r.drops -.09, .09
 
 
 
-###### Affective
+### Looking at alpha all together
+#### Substantive
+##### Affective
 psych::alpha(together[,c(2,5,6,7,10,13)])
 
-###### Behavioral
-psych::alpha(together[,c(14,15,18,19,22,23)])
+##### Behavioral
+psych::alpha(together[,c(14,15,19,20,22,23)]) #alpha -.71
+psych::alpha(together[,c(16,17,19,20,22,23)]) #alpha -.69
 
-###### Cognitive
-psych::alpha(together[,c(26,29,30,32,35,37)])
-
-###### All together
-psych::alpha(together[,c(2,5,6,7,10,13,14,15,18,19,22,23,26,29,30,32,35,37)])
+##### Cognitive
+psych::alpha(together[,c(26,29,31,33,35,37)]) 
 
 
-#### CFA
-Bifactor_Model2<-'
-Cognitive=~Item_26+Item_29+Item_30+Item_32+Item_35+Item_37
-Affective=~Item_2+Item_5+Item_6+Item_7+Item_10+Item_13
-Behavioral=~Item_14+Item_15+Item_18+Item_19+Item_22+Item_23
-Absorption=~Item_2+Item_5+Item_14+Item_15+Item_35+Item_36
-Vigor=~Item_6+Item_7+Item_18+Item_19+Item_26+Item_29
-Dedication=~Item_10+Item_13+Item_22+Item_23+Item_35+Item_36
-Cognitive ~~ Affective
-Cognitive ~~ Behavioral
-Affective ~~ Behavioral
-Absorption ~~ Vigor
-Absorption ~~ Dedication
-Vigor ~~ Dedication'
+#### Attitudinal
+##### Absorption
+psych::alpha(together[,c(2,5,14,15,35,37)]) #alpha -.72
+psych::alpha(together[,c(2,5,16,17,35,37)]) #alpha -.61
+##### Dedication
+psych::alpha(together[,c(10,13,22,23,31,33)]) 
+##### Vigor
+psych::alpha(together[,c(6,7,19,20,26,29)]) 
 
-Fit.Bi <- lavaan::cfa(Bifactor_Model2, data = together, orthogonal=TRUE)
 
-semPlot::semPaths(Fit.Bi, bifactor = c("Cognitive", "Affective", "Behavioral"), "std", layout = "tree3", 
+##### all together
+psych::alpha(together[,c(2,5,6,7,10,13,14,15,19,20,22,23,26,29,31,33,35,37)])
+
+Reduction_2Items<-together[,c(2,5,6,7,10,13,14,15,19,20,22,23,26,29,31,33,35,37)]
+
+Reduction_2Items<-Reduction_2Items%>%rename(
+ # Item_1=`Iâ€™m able to concentrate on my work without distractions.`,
+  Item_2=`I have a hard time detaching mentally from my work.`,
+ # Item_3=`Time passes quickly while Iâ€™m working.`,
+  Item_4=`I find it difficult to mentally disconnect from work.`,
+  Item_5=`I enjoy thinking about work even when Iâ€™m not at work.`,
+ # Item_6=`Most days, I feel happiest when the workday is soon to be complete.`,
+ # Item_7=`I am happiest when I am immersed in a project.`,
+  Item_8=`I love starting my workday.`,
+  Item_9=`I devote more time than is expected of me.`,
+  Item_10=`I have to be reminded to take breaks while Iâ€™m at work.`,
+ # Item_11=`I never miss a work deadline.`,
+ # Item_12=`I never allow distractions to interfere with my work.`,
+  Item_13=`I devote my full attention to my work tasks throughout the day.`,
+ # Item_14=`Thinking about work saps my energy.`,
+ # Item_15=`I would rather direct my focus toward a work task than a personal task.`,
+  Item_16=`Iâ€™m able to maintain good levels of energy throughout the workday.`,
+  Item_17=`I enjoy spending time completing my job tasks.`,
+  Item_18=`Most days I feel enthusiastic about starting my work day.`,
+ # Item_19=`I feel motivated to go beyond what is asked of me.`,
+ # Item_20=`This job drains my energy.`,
+ # Item_21=`When work is slow I find ways to be productive.`,
+  Item_22=`I express enthusiasm for my job while at work.`,
+  Item_23=`I try my best to perform well at work.`,
+ # Item_24=`If I notice my energy level is low, I take corrective steps to re-energize.`,
+ # Item_25=`I plan my future with this company.`,
+  Item_26=`I believe this company cares about my career goals.`,
+ # Item_27=`I often think about finding another job.`,
+  Item_28=`This organization challenges me to work at my full potential.`,
+  Item_29=`I am proud to be a member of this organization.`,
+ # Item_30=`I feel supported by my supervisor when I fail at a task.`,
+ # Item_31=`I feel proud of my accomplishments within this organization.`,
+  Item_32=`My job makes me feel like Iâ€™m part of something meaningful.`,
+  Item_33=`I make valued contributions to the organization.`,
+  Item_34=`I embrace challenging situations at work.`,
+ # Item_35=`I speak positively about this organization to others.`,
+ # Item_36=`This organization provides the resources necessary for me to successfully perform my job.`
+)
+
+Sub_Model_ItemReduction2<-'
+Absorption=~Item_2+Item_4+Item_5+Item_8+Item_9+Item_10
+Vigor=~Item_13+Item_16+Item_17+Item_18+Item_22+Item_23
+Dedication=~Item_26+Item_28+Item_29+Item_32+Item_33+Item_34
+'
+
+Fit1.4<-lavaan::cfa(Sub_Model_ItemReduction2, data = Reduction_2Items)
+semPlot::semPaths(Fit1.4,"std")
+lavaan::fitMeasures(Fit1.4)
+summary(Fit1.4, fit.measure=TRUE)
+
+Att_Model_ItemReduction2<-'
+Cognitive=~Item_2+Item_4+Item_13+Item_16+Item_26+Item_28
+Affective=~Item_5+Item_8+Item_17+Item_18+Item_29+Item_32
+Behavioral=~Item_9+Item_10+Item_22+Item_23+Item_33+Item_34
+'
+Fit1.5<-lavaan::cfa(Att_Model_ItemReduction2, data = Reduction_2Items)
+semPlot::semPaths(Fit1.5,"std")
+lavaan::fitMeasures(Fit1.5)
+summary(Fit1.5, fit.measure=TRUE)
+
+
+#### Bifactor model 2 items
+Bifactor_Model3<-'
+Cognitive=~Item_2+Item_4+Item_13+Item_16+Item_26+Item_28
+Affective=~Item_5+Item_8+Item_17+Item_18+Item_29+Item_32
+Behavioral=~Item_9+Item_10+Item_22+Item_23+Item_33+Item_34
+Absorption=~Item_2+Item_4+Item_5+Item_8+Item_9+Item_10
+Vigor=~Item_13+Item_16+Item_17+Item_18+Item_22+Item_23
+Dedication=~Item_26+Item_28+Item_29+Item_32+Item_33+Item_34
+Absorption ~~ 0*Affective
+Absorption ~~ 0*Behavioral
+Absorption ~~ 0*Cognitive
+Vigor ~~ 0*Affective
+Vigor ~~ 0*Behavioral
+Vigor ~~ 0*Cognitive
+Dedication ~~ 0*Affective
+Dedication ~~ 0*Behavioral
+Dedication ~~ 0*Cognitive
+'
+
+
+Fit.Bi3 <- lavaan::cfa(Bifactor_Model3, data = Reduction_2Items) #, likelihood = "wishart")
+
+semPlot::semPaths(Fit.Bi3, bifactor = c("Cognitive", "Affective", "Behavioral"), "std", layout = "tree3", 
                   rotation = 2, curvePivot=TRUE, style="lisrel", nCharNodes = 0)  ## exoCov=FALSE deletes all covariances
 title("Initial pilot bifactor analysis (36 candidate items)")
 
-summary(Fit.Bi)
+modindices(Fit.Bi3, sort = TRUE, maximum.number = 5)
 
-
-
-
+summary(Fit.Bi3)
