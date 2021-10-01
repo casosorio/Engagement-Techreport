0c33e6c31e1f77156a6df18089d2c3895924ef67
casosorio <you@example.com>
2021-10-01 12:36:31 -0400

    measurement of invariance

--- Scripts/Measurment of Invariance.R
+++ Scripts/Measurment of Invariance.R
@@ -60,6 +60,10 @@ together$`This job drains my energy.` <- 7 - together$`This job drains my energy
 together$`Thinking about work saps my energy.` <- 7 - together$`Thinking about work saps my energy.`
 together$`I often think about finding another job.` <- 7 - together$`I often think about finding another job.`
 
+
+###Condition 1
+
+
 Cond1_Model<-cond1.red[,2:37]
 
 Cond1_Model<-Cond1_Model%>%rename(
@@ -102,7 +106,26 @@ Cond1_Model<-Cond1_Model%>%rename(
 ) %>% 
   select(Item_1,Item_2,Item_3,Item_4,Item_5,Item_6,Item_7,Item_8,Item_9,Item_10,Item_11,Item_12,Item_13,Item_14,Item_15,Item_16,Item_17,
          Item_18,Item_19,Item_20,Item_21,Item_22,Item_23,Item_24,Item_25,Item_26,Item_27,Item_28,Item_29,Item_30,Item_31,Item_32,Item_33,
-         Item_34,Item_35,Item_36)
+         Item_34,Item_35,Item_36) %>% 
+  mutate(across(c("Item_1":"Item_36"),~as.numeric(.)))
+
+## Absorption - Cognitive 
+psych::alpha(Cond1_Model[,c(1,2,3)],check.keys = T) #raw alpha-.32, std.alpha-.36, r.drops: .142, .079, .369
+psych::alpha(Cond1_Model[,c(1,2,4)],check.keys = T) #raw alpha-.63, std.alpha-.61, r.drops: .045, .709, .702
+psych::alpha(Cond1_Model[,c(1,3,4)],check.keys = T) #raw alpha-.27, std.alpha-.31, r.drops: .157, .296, .033
+psych::alpha(Cond1_Model[,c(2,3,4)],check.keys = T) #raw alpha-.71, std.alpha-.68, r.drops: .82, .15, .74 - Best R Drops
+
+
+
+
+
+
+
+
+
+
+
+
 
 ######Bifactor model
 Bifactor_Model_Cond1<-'
@@ -128,6 +151,86 @@ Fit.Bi_Cond1 <- lavaan::cfa(Bifactor_Model_Cond1, data = Cond1_Model) #, likelih
 semPlot::semPaths(Fit.Bi_Cond1, bifactor = c("Cognitive", "Affective", "Behavioral"), "std", layout = "tree3", 
                   rotation = 2, curvePivot=TRUE, style="lisrel", nCharNodes = 0)  ## exoCov=FALSE deletes all covariances
 title("Initial pilot bifactor analysis (36 candidate items)")
+
+
+
+## Absorption - Cognitive 
+psych::alpha(Cond1_Model[,c(1,2,3)],check.keys = T) #raw alpha-.39, std.alpha-.42, r.drops: .25, .089, .391
+psych::alpha(Cond1_Model[,c(1,2,4)],check.keys = T) #raw alpha-.59, std.alpha-.56, r.drops: .007, .65, .677
+psych::alpha(Cond1_Model[,c(1,3,4)],check.keys = T) #raw alpha-.33, std.alpha-.37, r.drops: .222, .351, .034
+psych::alpha(Cond1_Model[,c(2,3,4)],check.keys = T) #raw alpha-.68, std.alpha-.65, r.drops: .77, .12, .71 
+
+
+## Absorption - Affective 
+psych::alpha(Cond1_Model[,c(5,6,7)],check.keys = T) #raw alpha-.55, std.alpha-.53, r.drops: .50, .40, .23  
+psych::alpha(Cond1_Model[,c(5,6,8)],check.keys = T) #raw alpha-.73, std.alpha-.73, r.drops: .62, .49, .58 
+psych::alpha(Cond1_Model[,c(5,7,8)],check.keys = T) #raw alpha-.65, std.alpha-.64, r.drops: .55, .31, .57 
+psych::alpha(Cond1_Model[,c(6,7,8)],check.keys = T) #raw alpha-.53, std.alpha-.52, r.drops: .35, .24, .47
+
+
+## Absoption - Behavioral
+psych::alpha(Cond1_Model[,c(9,10,11)],check.keys = T) #raw alpha-.36, std.alpha-.35, r.drops: .24, .37, .071 
+psych::alpha(Cond1_Model[,c(9,10,12)],check.keys = T) #raw alpha-.49, std.alpha-.49, r.drops: .44, .40, .32 
+psych::alpha(Cond1_Model[,c(9,11,12)],check.keys = T) #raw alpha-.46, std.alpha-.46, r.drops: .20, .23, .44  
+psych::alpha(Cond1_Model[,c(10,11,12)],check.keys = T) #raw alpha-.47, std.alpha-.48, r.drops: .23, .29, .38
+
+
+## Vigor - Cognitive
+psych::alpha(Cond1_Model[,c(13,14,15)],check.keys = T) #raw alpha-.41, std.alpha-.41, r.drops: .27, .18, .28 
+psych::alpha(Cond1_Model[,c(13,14,16)],check.keys = T) #raw alpha-.62, std.alpha-.62, r.drops: .35, .33, .62 
+psych::alpha(Cond1_Model[,c(13,15,16)],check.keys = T) #raw alpha-.61, std.alpha-.62, r.drops: .47, .33, .48  # Best R drops
+psych::alpha(Cond1_Model[,c(14,15,16)],check.keys = T) #raw alpha-.55, std.alpha-.56, r.drops: .36, .25, .49
+
+
+## Vigor - Affective
+psych::alpha(Cond1_Model[,c(17,18,19)],check.keys = T) #raw alpha-.74, std.alpha-.75, r.drops: .65, .56, .53 # Best R drops
+psych::alpha(Cond1_Model[,c(17,18,20)],check.keys = T) #raw alpha-.66, std.alpha-.67, r.drops: .48, .60, .37
+psych::alpha(Cond1_Model[,c(17,19,20)],check.keys = T) #raw alpha-.55, std.alpha-.57, r.drops: .48, .41, .23
+psych::alpha(Cond1_Model[,c(18,19,20)],check.keys = T) #raw alpha-.60, std.alpha-.60, r.drops: .54, .36, .35
+
+## Vigor - Behavioral
+psych::alpha(Cond1_Model[,c(21,22,23)],check.keys = T) #raw alpha-.69, std.alpha-.71, r.drops: .50, .53, .54 # Best R drops
+psych::alpha(Cond1_Model[,c(21,22,24)],check.keys = T) #raw alpha-.63, std.alpha-.63, r.drops: .46, .47, .39
+psych::alpha(Cond1_Model[,c(21,23,24)],check.keys = T) #raw alpha-.60, std.alpha-.62, r.drops: .45, .45, .37
+psych::alpha(Cond1_Model[,c(22,23,24)],check.keys = T) #raw alpha-.62, std.alpha-.64, r.drops: .48, .49, .37
+
+
+## Dedication - Cognitive
+psych::alpha(Cond1_Model[,c(25,26,27)],check.keys = T) #raw alpha-.80, std.alpha-.80, r.drops: .67, .61, .65
+psych::alpha(Cond1_Model[,c(25,26,28)],check.keys = T) #raw alpha-.80, std.alpha-.81, r.drops: .59, .70, .66 # Best R drops
+psych::alpha(Cond1_Model[,c(25,27,28)],check.keys = T) #raw alpha-.77, std.alpha-.78, r.drops: .66, .64, .55
+psych::alpha(Cond1_Model[,c(26,27,28)],check.keys = T) #raw alpha-.79, std.alpha-.79, r.drops: .69, .56, .66
+
+# 25, 26, 27 might be another good option since item 27 is reverse coded item
+
+## Dedication - Affective
+psych::alpha(Cond1_Model[,c(29,30,31)],check.keys = T) #raw alpha-.62, std.alpha-.64, r.drops: .49, .35, .50
+psych::alpha(Cond1_Model[,c(29,30,32)],check.keys = T) #raw alpha-.68, std.alpha-.69, r.drops: .56, .36, .59
+psych::alpha(Cond1_Model[,c(29,31,32)],check.keys = T) #raw alpha-.80, std.alpha-.80, r.drops: .65, .61, .68 # Best R Drops
+psych::alpha(Cond1_Model[,c(30,31,32)],check.keys = T) #raw alpha-.65, std.alpha-.67, r.drops: .37, .52, .54
+
+## Dedication - Behavioral
+psych::alpha(Cond1_Model[,c(33,34,35)],check.keys = T) #raw alpha-.60, std.alpha-.63, r.drops: .43, .49, .37 
+psych::alpha(Cond1_Model[,c(33,34,36)],check.keys = T) #raw alpha-.50, std.alpha-.55, r.drops: .36, .42, .23
+psych::alpha(Cond1_Model[,c(33,35,36)],check.keys = T) #raw alpha-.56, std.alpha-.55, r.drops: .25, .49, .41
+psych::alpha(Cond1_Model[,c(34,35,36)],check.keys = T) #raw alpha-.61, std.alpha-.61, r.drops: .35, .51, .43 # Best R Drops
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Condition2 _________________________________________________________________________________________
 Cond2_Model<-cond2.red[,2:37]
@@ -338,3 +441,189 @@ Fit.Bi_Cond4 <- lavaan::cfa(Bifactor_Model_Cond4, data = Cond4_Model) #, likelih
 semPlot::semPaths(Fit.Bi_Cond4, bifactor = c("Cognitive", "Affective", "Behavioral"), "std", layout = "tree3", 
                   rotation = 2, curvePivot=TRUE, style="lisrel", nCharNodes = 0)  ## exoCov=FALSE deletes all covariances
 title("Condition2 bifactor analysis (36 candidate items)")
+
+
+
+
+
+
+
+# Reneta's 18-Items______________________________________________________
+library(lavaan)
+Bifactor_modified <-'
+Absorption = ~Item_1 + Item_3  + Item_5  + Item_8  + Item_10 + Item_11
+Vigor      = ~Item_14 + Item_16 + Item_17 + Item_19 + Item_21 + Item_22
+Dedication = ~ Item_26 + Item_28 + Item_31 + Item_32 + Item_34 + Item_35
+Cognitive  = ~Item_1  + Item_3  + Item_14 + Item_16 + Item_26 + Item_28
+Affective  = ~Item_5 +  Item_8  + Item_17 + Item_19 + Item_31 + Item_32
+Behavioral = ~Item_10 + Item_11 + Item_21 + Item_22 + Item_34 + Item_35
+Absorption ~~ 0*Affective
+Absorption ~~ 0*Behavioral
+Absorption ~~ 0*Cognitive
+Vigor      ~~ 0*Affective
+Vigor      ~~ 0*Behavioral
+Vigor      ~~ 0*Cognitive
+Dedication ~~ 0*Affective
+Dedication ~~ 0*Behavioral
+Dedication ~~ 0*Cognitive
+'
+
+Cond1_Bi <- lavaan::cfa(Bifactor_modified_Cond1, data = Cond1_Model, missing = "ML", estimator = 'MLR')
+
+semPlot::semPaths(Cond1_Bi, bifactor = c("Cognitive", "Affective", "Behavioral"), "std", layout = "tree3",
+                  rotation = 2, curvePivot=TRUE, style="lisrel", nCharNodes = 0, pastel=TRUE)
+
+modindices(Cond1_Bi, sort = TRUE, maximum.number = 25)
+
+summary(Cond1_Bi, standardized=TRUE)
+
+
+
+Sub_Model<-'Absorption = ~Item_1 + Item_3  + Item_5  + Item_8  + Item_10 + Item_11
+Vigor= ~Item_14 + Item_16 + Item_17 + Item_19 + Item_21 + Item_22
+Dedication = ~ Item_26 + Item_28 + Item_31 + Item_32 + Item_34 + Item_35
+'
+
+Sub_Cond1<-lavaan::cfa(Sub_Model, data = Cond1_Model, missing = "ML", estimator = 'MLR')
+semPlot::semPaths(Sub_Cond1, "std")
+modindices(Sub_Cond1, sort = TRUE, maximum.number = 25)
+summary(Sub_Cond1,standardized = TRUE)
+
+Att_Model<-'Cognitive  = ~Item_1  + Item_3  + Item_14 + Item_16 + Item_26 + Item_28
+Affective  = ~Item_5 +  Item_8  + Item_17 + Item_19 + Item_31 + Item_32
+Behavioral = ~Item_10 + Item_11 + Item_21 + Item_22 + Item_34 + Item_35
+'
+
+Att_Con1<-lavaan::cfa(Att_Model, data = Cond1_Model, missing = "ML", estimator = 'MLR')
+semPlot::semPaths(Att_Con1, "std")
+modindices(Att_Con1, sort = TRUE, maximum.number = 25)
+summary(Att_Con1,standardized = TRUE)
+
+
+
+
+
+
+
+
+
+
+#Condition 2 __________________________________________________
+Bifactor_modified<-'
+Absorption = ~Item_1 + Item_3  + Item_5  + Item_8  + Item_10 + Item_11
+Vigor      = ~Item_14 + Item_16 + Item_17 + Item_19 + Item_21 + Item_22
+Dedication = ~ Item_26 + Item_28 + Item_31 + Item_32 + Item_34 + Item_35
+Cognitive  = ~Item_1  + Item_3  + Item_14 + Item_16 + Item_26 + Item_28
+Affective  = ~Item_5 +  Item_8  + Item_17 + Item_19 + Item_31 + Item_32
+Behavioral = ~Item_10 + Item_11 + Item_21 + Item_22 + Item_34 + Item_35
+Absorption ~~ 0*Affective
+Absorption ~~ 0*Behavioral
+Absorption ~~ 0*Cognitive
+Vigor      ~~ 0*Affective
+Vigor      ~~ 0*Behavioral
+Vigor      ~~ 0*Cognitive
+Dedication ~~ 0*Affective
+Dedication ~~ 0*Behavioral
+Dedication ~~ 0*Cognitive
+'
+
+Cond2_Bi <- lavaan::cfa(Bifactor_modified, data = Cond2_Model, missing = "ML", estimator = 'MLR')
+
+semPlot::semPaths(Cond2_Bi, bifactor = c("Cognitive", "Affective", "Behavioral"), "std", layout = "tree3",
+                  rotation = 2, curvePivot=TRUE, style="lisrel", nCharNodes = 0, pastel=TRUE)
+
+modindices(Cond2_Bi, sort = TRUE, maximum.number = 25)
+
+summary(Cond2_Bi, standardized=TRUE)
+
+Sub_Cond2<-lavaan::cfa(Sub_Model, data = Cond2_Model, missing = "ML", estimator = 'MLR')
+semPlot::semPaths(Sub_Cond2, "std")
+modindices(Sub_Cond2, sort = TRUE, maximum.number = 25)
+summary(Sub_Cond2,standardized = TRUE)
+
+
+Att_Con2<-lavaan::cfa(Att_Model, data = Cond2_Model, missing = "ML", estimator = 'MLR')
+semPlot::semPaths(Att_Con2, "std")
+modindices(Att_Con2, sort = TRUE, maximum.number = 25)
+summary(Att_Con2,standardized = TRUE)
+
+
+# Condition 3 ________________________________________________________________
+Bifactor_modified<-'
+Absorption = ~Item_1 + Item_3  + Item_5  + Item_8  + Item_10 + Item_11
+Vigor      = ~Item_14 + Item_16 + Item_17 + Item_19 + Item_21 + Item_22
+Dedication = ~ Item_26 + Item_28 + Item_31 + Item_32 + Item_34 + Item_35
+Cognitive  = ~Item_1  + Item_3  + Item_14 + Item_16 + Item_26 + Item_28
+Affective  = ~Item_5 +  Item_8  + Item_17 + Item_19 + Item_31 + Item_32
+Behavioral = ~Item_10 + Item_11 + Item_21 + Item_22 + Item_34 + Item_35
+Absorption ~~ 0*Affective
+Absorption ~~ 0*Behavioral
+Absorption ~~ 0*Cognitive
+Vigor      ~~ 0*Affective
+Vigor      ~~ 0*Behavioral
+Vigor      ~~ 0*Cognitive
+Dedication ~~ 0*Affective
+Dedication ~~ 0*Behavioral
+Dedication ~~ 0*Cognitive
+'
+
+Cond3_Bi <- lavaan::cfa(Bifactor_modified, data = Cond3_Model, missing = "ML", estimator = 'MLR')
+
+semPlot::semPaths(Cond3_Bi, bifactor = c("Cognitive", "Affective", "Behavioral"), "std", layout = "tree3",
+                  rotation = 2, curvePivot=TRUE, style="lisrel", nCharNodes = 0, pastel=TRUE)
+
+modindices(Cond3_Bi, sort = TRUE, maximum.number = 25)
+
+summary(Cond3_Bi, standardized=TRUE)
+
+
+Sub_Cond3<-lavaan::cfa(Sub_Model, data = Cond3_Model, missing = "ML", estimator = 'MLR')
+semPlot::semPaths(Sub_Cond3, "std")
+modindices(Sub_Cond3, sort = TRUE, maximum.number = 25)
+summary(Sub_Cond3,standardized = TRUE)
+
+
+Att_Con3<-lavaan::cfa(Att_Model, data = Cond3_Model, missing = "ML", estimator = 'MLR')
+semPlot::semPaths(Att_Con3, "std")
+modindices(Att_Con3, sort = TRUE, maximum.number = 25)
+summary(Att_Con3,standardized = TRUE)
+
+
+#Condition 4__________________________________________________________________
+Bifactor_modified<-'
+Absorption = ~Item_1 + Item_3  + Item_5  + Item_8  + Item_10 + Item_11
+Vigor      = ~Item_14 + Item_16 + Item_17 + Item_19 + Item_21 + Item_22
+Dedication = ~ Item_26 + Item_28 + Item_31 + Item_32 + Item_34 + Item_35
+Cognitive  = ~Item_1  + Item_3  + Item_14 + Item_16 + Item_26 + Item_28
+Affective  = ~Item_5 +  Item_8  + Item_17 + Item_19 + Item_31 + Item_32
+Behavioral = ~Item_10 + Item_11 + Item_21 + Item_22 + Item_34 + Item_35
+Absorption ~~ 0*Affective
+Absorption ~~ 0*Behavioral
+Absorption ~~ 0*Cognitive
+Vigor      ~~ 0*Affective
+Vigor      ~~ 0*Behavioral
+Vigor      ~~ 0*Cognitive
+Dedication ~~ 0*Affective
+Dedication ~~ 0*Behavioral
+Dedication ~~ 0*Cognitive
+'
+
+Cond4_Bi <- lavaan::cfa(Bifactor_modified, data = Cond4_Model, missing = "ML", estimator = 'MLR')
+
+semPlot::semPaths(Cond4_Bi, bifactor = c("Cognitive", "Affective", "Behavioral"), "std", layout = "tree3",
+                  rotation = 2, curvePivot=TRUE, style="lisrel", nCharNodes = 0, pastel=TRUE)
+
+modindices(Cond4_Bi, sort = TRUE, maximum.number = 25)
+
+summary(Cond4_Bi, standardized=TRUE)
+
+Sub_Cond4<-lavaan::cfa(Sub_Model, data = Cond4_Model, missing = "ML", estimator = 'MLR')
+semPlot::semPaths(Sub_Cond4, "std")
+modindices(Sub_Cond4, sort = TRUE, maximum.number = 25)
+summary(Sub_Cond4,standardized = TRUE)
+
+
+Att_Con4<-lavaan::cfa(Att_Model, data = Cond4_Model, missing = "ML", estimator = 'MLR')
+semPlot::semPaths(Att_Con4, "std")
+modindices(Att_Con4, sort = TRUE, maximum.number = 25)
+summary(Att_Con4,standardized = TRUE)
